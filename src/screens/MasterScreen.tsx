9a0c6ae1658be098afa87ac655e5dc2ce1a508b6
joshua-augustinus <joshua.augustinus@gmail.com>
2020-11-11 09:03:28 +1100

    Add Markdown

--- src/screens/MasterScreen.tsx
+++ src/screens/MasterScreen.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Button, Text, TextInput, TouchableOpacity, View, BackHandler } from 'react-native';
 import { SafeAreaView, StackActions } from 'react-navigation';
 import { DrawerActions, NavigationDrawerProp } from 'react-navigation-drawer';
+import Markdown from 'react-native-markdown-display';
 
 /**
  * https://reactnavigation.org/docs/4.x/typescript
@@ -9,6 +10,17 @@ import { DrawerActions, NavigationDrawerProp } from 'react-navigation-drawer';
 type Props = {
     navigation: NavigationDrawerProp<{ userId: string, routeName: string }>;
 }
+
+const copy = `
+Unordered
+
+- Sub-lists are made by indenting 2 spaces:
+  - Nested List
+    - Test
+    - Test
+    - Test
+- Very easy!
+`;
 
 const MasterScreen = (props: Props) => {
 
@@ -40,17 +52,15 @@ const MasterScreen = (props: Props) => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={{ height: 50, backgroundColor: 'red', flexDirection: 'row', alignItems: 'center' }}>
-
                 <TouchableOpacity style={{ backgroundColor: 'yellow' }}
                     onPress={() => onMenuPress()}>
                     <Text>Menu</Text>
                 </TouchableOpacity>
             </View>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-                <Text>{props.navigation.state.routeName}</Text>
-                <TextInput placeholder="Enter text here..."></TextInput>
-                <Button title="Press me" onPress={() => onButtonPress()}></Button>
+            <View style={{ flex: 1, width: '100%', backgroundColor: 'red' }}>
+                <Markdown >
+                    {copy}
+                </Markdown>
             </View>
         </SafeAreaView>
 
